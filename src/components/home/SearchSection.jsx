f32cb2f7f865d4d32041f0d2af3e66f089c42d17
IBRA-oub <brahimbourih@gmail.com>
2024-10-02 22:15:30 +0100

    feat : search section / responsive

---
+++ src/components/home/SearchSection.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+export default function SearchSection() {
+    return (
+        <>
+            <form action="">
+                <div className='bg-[#121212] h-40 md:h-20 w-full md:flex md:justify-center md:items-center'>
+
+                    <div className=' w-full h-1/2 flex justify-center items-center'>
+                        <input className='w-80 md:w-[90%] h-10 rounded pl-5 bg-[#252525]' type="text" placeholder='Titre...' />
+                    </div>
+                    <div className=' w-full  h-1/2 flex justify-evenly items-center'>
+
+                        <select name="" id="" className='w-20 md:w-48 h-10 rounded text-gray-400 px-2 bg-[#252525]'>
+                            <option value="">genre1</option>
+                            <option value="">genre2</option>
+                            <option value="">genre3</option>
+                            <option value="">genre4</option>
+                        </select>
+
+                        <input type="date" name="" id="" className='w-20 md:w-48 h-10 rounded text-gray-400  px-2 bg-[#252525]'/>
+                        <button type='submit' className=" text-white w-20 md:w-48 h-10  bg-[#FF1B1F] font-bold  py-1 rounded hover:bg-[#ff4f51]">connect</button>
+                    </div>
+                </div>
+            </form>
+
+        </>
+    )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import HeroSection from '../components/home/HeroSection'
+import SearchSection from '../components/home/SearchSection'
 
 export default function Home() {
     return (
         <>
             <HeroSection/>
+            <SearchSection/>
 
 
         </>
