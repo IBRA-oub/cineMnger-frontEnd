6ba63ea649f2afe2c4cf2762401942de6015f3cf
IBRA-oub <brahimbourih@gmail.com>
2024-10-07 14:17:42 +0100

    feat : ajouter logout function

--- src/components/Client/Navbar.jsx
+++ src/components/Client/Navbar.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import backHome from '../../assets/images/back-home.png';
+import { useNavigate } from 'react-router-dom';
+
 
 export default function Navbar() {
+
+  const navigate = useNavigate();
+
+  function logoutFunction() {
+
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
+    navigate("/")
+
+  }
+
+
+
   return (
     <>
       {/* for desktop */}
@@ -22,7 +37,7 @@ export default function Navbar() {
           <Link to="/client-tickets" className='w-14 h-14 rounded-lg bg-[#25252552] mr-2 flex justify-center items-center hover:bg-[#2525258f]'>
             <svg xmlns="http://www.w3.org/2000/svg" height="30" width="60" viewBox="0 0 576 512"><path fill="#000000" d="M64 64C28.7 64 0 92.7 0 128l0 64c0 8.8 7.4 15.7 15.7 18.6C34.5 217.1 48 235 48 256s-13.5 38.9-32.3 45.4C7.4 304.3 0 311.2 0 320l0 64c0 35.3 28.7 64 64 64l448 0c35.3 0 64-28.7 64-64l0-64c0-8.8-7.4-15.7-15.7-18.6C541.5 294.9 528 277 528 256s13.5-38.9 32.3-45.4c8.3-2.9 15.7-9.8 15.7-18.6l0-64c0-35.3-28.7-64-64-64L64 64zm64 112l0 160c0 8.8 7.2 16 16 16l288 0c8.8 0 16-7.2 16-16l0-160c0-8.8-7.2-16-16-16l-288 0c-8.8 0-16 7.2-16 16zM96 160c0-17.7 14.3-32 32-32l320 0c17.7 0 32 14.3 32 32l0 192c0 17.7-14.3 32-32 32l-320 0c-17.7 0-32-14.3-32-32l0-192z" /></svg>
           </Link>
-          <div className='w-14 h-14 bg-[#25252552] rounded-lg mr-2 flex justify-center items-center hover:bg-[#2525258f]'>
+          <div onClick={logoutFunction} className='w-14 h-14 bg-[#25252552] rounded-lg mr-2 flex justify-center items-center hover:bg-[#2525258f]'>
             <svg xmlns="http://www.w3.org/2000/svg" height="32" width="60" viewBox="0 0 512 512"><path fill="#000000" d="M377.9 105.9L500.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L377.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1-128 0c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM160 96L96 96c-17.7 0-32 14.3-32 32l0 256c0 17.7 14.3 32 32 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-64 0c-53 0-96-43-96-96L0 128C0 75 43 32 96 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32z" /></svg>
           </div>
         </div>
@@ -50,7 +65,7 @@ export default function Navbar() {
           </svg>
         </Link>
 
-        <div className='w-7 h-7  mr-2 flex justify-center items-center hover:bg-[#2525258f]'>
+        <div onClick={logoutFunction} className='w-7 h-7  mr-2 flex justify-center items-center hover:bg-[#2525258f]'>
           <svg xmlns="http://www.w3.org/2000/svg" height="32" width="60" viewBox="0 0 512 512">
             <path fill="#ffffff" d="M377.9 105.9L500.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L377.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1-128 0c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM160 96L96 96c-17.7 0-32 14.3-32 32l0 256c0 17.7 14.3 32 32 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-64 0c-53 0-96-43-96-96L0 128C0 75 43 32 96 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32z" />
           </svg>
